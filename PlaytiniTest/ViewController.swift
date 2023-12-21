1ec8b897873fe0fcbb8253a4bf1e3f13af4a0759
MelnykovMykyta <melnikovnikita@ukr.net>
2023-12-21 16:57:37 +0200

    Initial Commit

---
+++ PlaytiniTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaytiniTest
+//
+//  Created by Nikita Melnikov on 21.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
